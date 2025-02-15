08e7afdfbcbbe6b50fc43236ce51c790f94a1d91
Ketan Agrawal <agrawal11ketan@gmail.com>
2025-02-15 14:06:27 +0530

    integrated amongus registration with backend

--- Artimas/src/pages/Among Us/AURegister.jsx
+++ Artimas/src/pages/Among Us/AURegister.jsx
@@ -8,7 +8,7 @@ const textVariants = {
 };
 
 const imageVariants = {
-  hidden: { clipPath: "inset(50% 0 50% 0)" }, 
+  hidden: { clipPath: "inset(50% 0 50% 0)" },
   visible: {
     clipPath: "inset(0 0 0 0)",
     transition: { duration: 1.2, ease: "easeInOut" },
@@ -21,6 +21,7 @@ const AURegister = ({ visible, onClose }) => {
   const [paymentScreenshot, setPaymentScreenshot] = useState(null);
   const [error, setError] = useState("");
 
+  const emailRegex = /^[a-zA-Z]+\.[a-zA-Z]+[0-9]{2}@PCCOEPUNE\.ORG$/;
   if (!visible) return null;
 
   const handleChange = (field, value) => {
@@ -28,24 +29,50 @@ const AURegister = ({ visible, onClose }) => {
   };
 
   const handleNextStep = () => {
-    if (!participant.name || !participant.college || !participant.dept || !participant.phone || !participant.email) {
+    if (!participant.name || !participant.college || !participant.dept || !participant.phone || !participant.email) { 
       setError("All fields are required!");
       return;
     }
+    else if (!emailRegex.test(participant.email)) {
+      setStep(2);
+      console.log(participant.email);
+      console.log(emailRegex.test(participant.email));
+    }
+    else{
+      handleSubmit();
+      setStep(3);
+    }
     setError("");
-    setStep(2);
   };
 
   const handleFileChange = (e) => {
     setPaymentScreenshot(e.target.files[0]);
   };
 
+  const handleSubmit = async () => {
+    const response = await fetch("https://f72b-2409-40c2-3050-6b58-caa-ed1c-7402-97f8.ngrok-free.app/Amoung_usR", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name: participant.name, college: participant.college, department: participant.dept, phone: participant.phone, email: participant.email }),
+    });
+
+    const result = await response.json();
+    console.log("Registered Successfully!");
+    console.log(result);
+  };
+
+  const handleSubmitStep2 = () => {
+    handleSubmit();
+    setStep(3);
+  };
   return (
     <div className="fixed inset-0 bg-opacity-30 backdrop-blur-sm flex justify-center items-center event">
-      <motion.div 
+      <motion.div
         initial="hidden"
         animate="visible"
-        variants={imageVariants} 
+        variants={imageVariants}
         className="w-[540px] h-[540px] md:w-[600px] md:h-[600px] rounded-lg shadow-lg flex flex-col justify-center items-center relative p-4"
         style={{
           backgroundImage: `url(${backgroundImage})`,
@@ -54,7 +81,7 @@ const AURegister = ({ visible, onClose }) => {
           backgroundRepeat: "no-repeat",
         }}>
         {step === 1 && (
-          <motion.div 
+          <motion.div
             variants={textVariants}
             className="w-56 md:w-72 mt-2 md:mt-0">
             <h3 className="text-black event font-bold mb-1 md:mb-2">Participant Details</h3>
@@ -107,7 +134,7 @@ const AURegister = ({ visible, onClose }) => {
         )}
 
         {step === 2 && (
-          <motion.div 
+          <motion.div
             variants={textVariants}
             className="w-64 md:w-72">
             <h3 className="text-black md:text-lg font-bold mb-2">Upload Payment Screenshot</h3>
@@ -119,9 +146,17 @@ const AURegister = ({ visible, onClose }) => {
               <p className="text-red-500 text-sm font-semibold">Please upload a screenshot</p>
             )}
 
-            <button className="w-64 md:w-72 mt-2 px-4 py-2 bg-red-500 text-black font-semibold rounded" onClick={onClose} disabled={!paymentScreenshot}>
+            <button className="w-64 md:w-72 mt-2 px-4 py-2 bg-red-500 text-black font-semibold rounded" onClick={handleSubmitStep2} disabled={!paymentScreenshot}>
               Submit
             </button>
+          </motion.div>
+        )}
+        {step === 3 && (
+          <motion.div
+            variants={textVariants}
+            className="w-64 md:w-72">
+
+            <h3 className="text-black md:text-lg font-bold mb-2">Registered Successfully!</h3>
           </motion.div>
         )}
       </motion.div>
